08472ac21e083710b9abf54741d85b9d7b573da3
anig <alexisnahuelidoyaga@gmail.com>
2026-03-25 14:21:36 -0300

    no me acuerdo que hice aca xd

---
+++ core/router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from apps.edificios.departamentos.controller import router as departamentos_router
+from apps.edificios.edificios.controller import router as edificios_router
+from apps.authentication.controller import router as authentication_router
+from apps.contratos.contratos.controller import router as contratos_router
+from apps.inquilinos.inquilinos.controller import router as inquilinos_router
+from apps.reclamos.reclamos.controller import router as reclamos_router
+app_router = APIRouter()
+app_router.include_router(edificios_router)
+app_router.include_router(departamentos_router)
+app_router.include_router(authentication_router)
+app_router.include_router(inquilinos_router)
+app_router.include_router(contratos_router)
+app_router.include_router(reclamos_router)
